57132f57faaa44fbff0ba3a8808e77d2bcd06565
Lucas Vieira <lucas@Seans-MacBook-Pro-2.local>
2019-04-03 11:07:35 +0100

    declaration file

---
+++ build/index.d.ts
@@ -0,0 +1,40 @@
+interface IUserDetailed extends IUser {
+    id: string;
+}
+interface IUserExtra {
+    [key: string]: string | number;
+}
+export interface IClient {
+    url: string;
+    username: string;
+    apiKey: string;
+    listId: string;
+    logging?: boolean;
+}
+export interface IUser {
+    email: string;
+    name: string;
+    tags: string[];
+    extra: IUserExtra;
+}
+export interface ICampaign {
+    title: string;
+    subject: string;
+    fromName: string;
+    replyTo: string;
+    templateId: number;
+}
+export declare class Client {
+    private authToken;
+    private url;
+    private listId;
+    private logging;
+    constructor(params: IClient);
+    private _requestHttp;
+    private _getUser;
+    getUser: (userEmail: string) => Promise<IUserDetailed>;
+    addOrGetUser: (user: IUser) => Promise<IUser>;
+    editUserTags: (userEmail: string, tags: string[]) => Promise<IUser>;
+    editUserExtra: (userEmail: string, userExtra: IUserExtra) => Promise<IUser>;
+}
+export {};
